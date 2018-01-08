9aa8bb3d6e94e9734c43fc4ad2954d840020b160
Kevin@test.com <kevin@test.com>
2018-01-08 12:41:19 +0000

    BlockingQueue moved

--- OOP-project/src/ie/gmit/sw/Launcher.java
+++ OOP-project/src/ie/gmit/sw/Launcher.java
@@ -1,14 +1,16 @@
 package ie.gmit.sw;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Launcher {
 	
 	Menu m = new Menu();
-	
+	BlockingQueue<Shingle> q = new LinkedBlockingQueue<>(m.getBlockingQSize());
 	public void launch(String f1, String f2) throws Exception{
-		BlockingQueue<Shingle> q = new LinkedBlockingQueue<>(m.getBlockingQSize());
+		
 		Thread t1 = new Thread(new DocumentParser(f1,q,m.getShingleSize(),m.getK()),"T1");
 		Thread t2 = new Thread(new DocumentParser(f2, q, m.getShingleSize(), m.getK()), "T2");
 		Thread t3 = new Thread(new Consumer( q,m.getK(),m.getThrPoolSize()),"T3");
@@ -18,5 +20,7 @@ public class Launcher {
 		t1.join();
 		t2.join();
 		t3.join();
+		
 	}
+	
 }
